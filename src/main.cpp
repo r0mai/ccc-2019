e68417a817a75691bb4f9041bb3d8b0e7a9124be
Andras Kucsma <andras.kucsma@prezi.com>
2019-11-08 15:14:14 +0100

    level 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 
 int main() {
-    std::cout << "asd" << std::endl;
+    long long rows, cols;
+    std::cin >> rows >> cols;
+    std::vector<long long> vals(rows*cols);
+    long long i = 0;
+    for (long long r = 0; r < rows; ++r) {
+        for (long long c = 0; c < cols; ++c) {
+            std::cin >> vals[i++];
+        }
+    }
+
+    long long min = *std::min_element(vals.begin(), vals.end());
+    long long max = *std::max_element(vals.begin(), vals.end());
+    long long sum = std::accumulate(vals.begin(), vals.end(), 0ll);
+    long long avg = sum / (long long)(vals.size());
+
+    std::cout << min << " " << max << " " << avg << std::endl;
 }
